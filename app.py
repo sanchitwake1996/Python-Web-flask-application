fcf33df1b5dea1a0bc1095039fd38c93a8650a12
sanchitwake1996 <sanchitwake1997@gmail.com>
2025-01-30 17:32:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def hello():
+    return "Hello from Flask running on Ubuntu via CodeDeploy!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
